83d6c522d75e3130393dbf3ff5ef578c9b22e144
noyeosa-obazee <obazeenoyeosa@gmail.com>
2025-11-11 13:05:35 +0100

    Add css styling and change division symbol

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ const clearAllButton = document.querySelector('#clear-all-btn');
 let expression = '';
 
 function operate(expr) {
-    const tokens = expr.replace(/\s+/g, '').split(/([+\-x/])/).filter(token => token);
+    const tokens = expr.replace(/\s+/g, '').split(/([+\-x÷])/).filter(token => token);
     let startIndex = 0;
     let result = 0;
 
@@ -41,7 +41,7 @@ function operate(expr) {
             case 'x':
                 result *= nextNumber;
                 break;
-            case '/':
+            case '÷':
                 if (nextNumber === 0) throw new Error('Division by zero');
                 result /= nextNumber;
                 break;
@@ -61,14 +61,15 @@ operationDisplay.textContent = expression
 
 operatorButton.forEach(function (btn) { 
 btn.addEventListener('click', function (e) {
-    if(!expression && (e.target.textContent === 'x' || e.target.textContent === '/')) {
+    if(!expression && (e.target.textContent === 'x' || e.target.textContent === '÷')) {
         return;
     }
-    else if(!/[x/+-\-]$/.test(expression)) {
+    else if(!/[x÷+-\-]$/.test(expression)) {
 
-        if(/[x/+\\-]/.test(expression)) {
-      const result = operate(expression);
-      operationResult.textContent = result;
+        if(/[x÷+\\-]/.test(expression)) {
+          const result = operate(expression);
+          operationResult.textContent = result;
+          expression = result;
         }
 
        expression +=e.target.textContent
